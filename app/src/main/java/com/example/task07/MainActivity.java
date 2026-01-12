c61cb4bd87aa37488c517763cb193caa890c836b
nitzandamri2 <nitzandamri@gmail.com>
2026-01-12 20:13:14 +0200

    final commit

--- app/src/main/java/com/example/task07/MainActivity.java
+++ app/src/main/java/com/example/task07/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.task07;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonClick = findViewById(R.id.buttonClick);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num = (int) (Math.random() * 3) + 1;
+                buttonClick.setText("" + num);
+                if (num == 1) {
+                    imageView.setImageResource(R.drawable.img);
+                }
+                else if (num == 2) {
+                    imageView.setImageResource(R.drawable.img_1);
+                }
+                else if (num == 3) {
+                    imageView.setImageResource(R.drawable.img_2);
+                }
+            }
+        });
     }
 }
